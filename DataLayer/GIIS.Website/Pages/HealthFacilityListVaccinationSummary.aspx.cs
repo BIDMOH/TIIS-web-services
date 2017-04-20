ac9795b72b74068f13ccb7d777ca42144c5c1f66
Martha Shaka <Martha Shaka>
2017-04-20 21:53:22 +0300

    vaccination summary

--- DataLayer/GIIS.Website/Pages/HealthFacilityListVaccinationSummary.aspx.cs
+++ DataLayer/GIIS.Website/Pages/HealthFacilityListVaccinationSummary.aspx.cs
@@ -85,176 +85,142 @@ public partial class Pages_HealthFacilityListVaccinationSummary : System.Web.UI.
     }
 
     protected void createInputControls(){
-        //date-from controls
-        inputControl3 = new HtmlGenericControl("select");
+                    //date-from controls
+                    inputControl3 = new HtmlGenericControl("select");
 
-        inputControl3.Attributes.Add("class", "form-control");
+                    inputControl3.Attributes.Add("class", "form-control");
 
-        var contextParms = new List<NpgsqlParameter>() {
-            new NpgsqlParameter("@FacilityId", NpgsqlTypes.NpgsqlDbType.Integer) { Value = CurrentEnvironment.LoggedUser.HealthFacilityId },
-            new NpgsqlParameter("@FacilityCode", NpgsqlTypes.NpgsqlDbType.Integer) { Value = CurrentEnvironment.LoggedUser.HealthFacility.Code },
-            new NpgsqlParameter("@UserId", NpgsqlTypes.NpgsqlDbType.Integer) { Value = CurrentEnvironment.LoggedUser.Id }
-        };
-        
-        // var params = new List<NpgsqlParameter>() {
-        //     new NpgsqlParameter("@FacilityId", NpgsqlTypes.NpgsqlDbType.Integer) { Value = CurrentEnvironment.LoggedUser.HealthFacilityId }
-        // };
-
-        hfParentID = HealthFacility.GetHealthFacilityById(CurrentEnvironment.LoggedUser.HealthFacilityId).ParentId;
+                    var contextParms = new List<NpgsqlParameter>() {
+                        new NpgsqlParameter("@FacilityId", NpgsqlTypes.NpgsqlDbType.Integer) { Value = CurrentEnvironment.LoggedUser.HealthFacilityId },
+                        new NpgsqlParameter("@FacilityCode", NpgsqlTypes.NpgsqlDbType.Integer) { Value = CurrentEnvironment.LoggedUser.HealthFacility.Code },
+                        new NpgsqlParameter("@UserId", NpgsqlTypes.NpgsqlDbType.Integer) { Value = CurrentEnvironment.LoggedUser.Id }
+                    };
 
 
-        string command="";
-
-        int userId = CurrentEnvironment.LoggedUser.Id;
-        UserRole role = UserRole.GetUserRoleByUserId(userId);
-        if(role.Role.Name.Equals("Middle Level Officer"))
-        {
-              command = "SELECT \"ID\", \"NAME\" FROM \"HEALTH_FACILITY\" ORDER BY \"NAME\" ";
-          }else{
-              command = "SELECT \"ID\", \"NAME\" FROM \"HEALTH_FACILITY\" ORDER BY \"NAME\" ";
-          }
+                    hfParentID = HealthFacility.GetHealthFacilityById(CurrentEnvironment.LoggedUser.HealthFacilityId).ParentId;
 
 
-        using (var idt = DBManager.ExecuteReaderCommand(command, System.Data.CommandType.Text, contextParms))
-        {
-            using (var irdr = idt.CreateDataReader())
-            {
-    
-                int count =0;
-                while (irdr.Read())
-                {
+                    string command="";
 
-                    var opt = new HtmlGenericControl("option");
-                    inputControl3.Controls.Add(opt);
-                    opt.Attributes.Add("value", irdr[0].ToString());
-                    opt.InnerText = irdr[1].ToString();
-
-                    if(selectedHealthFacilityID.Equals(irdr[0].ToString())){
-                        opt.Attributes.Add("selected","true");
+                    int userId = CurrentEnvironment.LoggedUser.Id;
+                    UserRole role = UserRole.GetUserRoleByUserId(userId);
+                     if(role.Role.Name.Equals("Middle Level Officer"))
+                    {
+                        command = "SELECT \"ID\", \"NAME\" FROM \"HEALTH_FACILITY\" ORDER BY \"NAME\" ";
+                    }else{
+                        command = "SELECT \"ID\", \"NAME\" FROM \"HEALTH_FACILITY\" ORDER BY \"NAME\" ";
                     }
 
-                    count++;
+
+                    using (var idt = DBManager.ExecuteReaderCommand(command, System.Data.CommandType.Text, contextParms))
+                    {
+                        using (var irdr = idt.CreateDataReader())
+                        {
+
+                            int count =0;
+                            while (irdr.Read())
+                            {
+
+                                var opt = new HtmlGenericControl("option");
+                                inputControl3.Controls.Add(opt);
+                                opt.Attributes.Add("value", irdr[0].ToString());
+                                opt.InnerText = irdr[1].ToString();
+
+                                if(selectedHealthFacilityID.Equals(irdr[0].ToString())){
+                                    opt.Attributes.Add("selected","true");
+                                }
+
+                                count++;
+                            }
+                        }
+                    }
+
+                    inputControl3.Attributes.Add("id", "selectHealthFacility");
+                    inputControl3.Attributes.Add("type", "text");
+                    inputControl3.Attributes.Add("style", "z-index:8");
+                    inputControl3.Attributes.Add("name", "selectHealthFacility");
+                    inputControl3.Attributes.Add("Visible", "false");
+                    inputControl3.Attributes.Add("title", "Health Facility option description");
+
+                    //date-from controls
+                    var inputControl = new HtmlGenericControl("input");
+                    inputControl.Attributes.Add("class", "form-control");
+
+                    inputControl.Attributes.Add("id", "dateFrom");
+                    inputControl.Attributes.Add("type", "text");
+                    inputControl.Attributes.Add("style", "z-index:8");
+                    inputControl.Attributes.Add("name", "dateFrom");
+                    inputControl.Attributes.Add("value", datefromString);
+                    inputControl.Attributes.Add("title", "Session Report Description");
+
+                    //date-to Controls
+                    var inputControl2 = new HtmlGenericControl("input");
+                    inputControl2.Attributes.Add("class", "form-control");
+
+                    inputControl2.Attributes.Add("id", "dateTo");
+                    inputControl2.Attributes.Add("type", "text");
+                    inputControl2.Attributes.Add("style", "z-index:8");
+                    inputControl2.Attributes.Add("name", "dateTo");
+                    inputControl2.Attributes.Add("value", datetoString);
+                    inputControl2.Attributes.Add("title", "Session Report Description");
+
+                    // Label control
+                    var labelControl = new Label()
+                    {
+                        Text = "From"
+                    };
+
+                        var labelControl2 = new Label()
+                    {
+                        Text = "To"
+                    };
+
+                        var labelControl3 = new Label()
+                    {
+                        Text = "Health Facility"
+                    };
+
+                    var row = new HtmlGenericControl("div");
+                    row.Attributes.Add("class", "row");
+                    // row.Attributes.Add("style", "margin:5px");
+
+                    var colMd4 = new HtmlGenericControl("div");
+                    colMd4.Attributes.Add("class", "col-md-1");
+                    var colMd42 = new HtmlGenericControl("div");
+                    colMd42.Attributes.Add("class", "col-md-1");
+                    var colMd43 = new HtmlGenericControl("div");
+                    colMd43.Attributes.Add("class", "col-md-1");
+
+                    var colMd8 = new HtmlGenericControl("div");
+                    colMd8.Attributes.Add("class", "col-md-3");
+                    var colMd82 = new HtmlGenericControl("div");
+                    colMd82.Attributes.Add("class", "col-md-3");
+                    var colMd83 = new HtmlGenericControl("div");
+                    colMd83.Attributes.Add("class", "col-md-3");
+
+                    row.Controls.Add(colMd4);
+                    row.Controls.Add(colMd8);
+                    row.Controls.Add(colMd42);
+                    row.Controls.Add(colMd82);
+                    row.Controls.Add(colMd43);
+                    row.Controls.Add(colMd83);
+
+                    colMd4.Controls.Add(labelControl);
+                    colMd8.Controls.Add(inputControl);
+                    colMd42.Controls.Add(labelControl2);
+                    colMd82.Controls.Add(inputControl2);
+                    colMd43.Controls.Add(labelControl3);
+                    colMd83.Controls.Add(inputControl3);
+
+                    this.reportInputs.Controls.Add(row);
+
+                    Page.RegisterStartupScript("dateFrom",
+                                string.Format(
+                                "<script type=\"text/javascript\">Sys.Application.add_init(function() {{$create(Sys.Extended.UI.CalendarBehavior, {{\"format\":\"MM-dd-yyyy\",\"id\":\"dateFrom\"}}, null, null, $get(\"{0}\"));}});</script>", "dateFrom"));
+                    Page.RegisterStartupScript("dateTo",
+                                string.Format(
+                                "<script type=\"text/javascript\">Sys.Application.add_init(function() {{$create(Sys.Extended.UI.CalendarBehavior, {{\"format\":\"MM-dd-yyyy\",\"id\":\"dateTo\"}}, null, null, $get(\"{0}\"));}});</script>", "dateTo"));
                 }
-            }
-        }
-
-        inputControl3.Attributes.Add("id", "selectHealthFacility");
-        inputControl3.Attributes.Add("type", "text");
-        inputControl3.Attributes.Add("style", "z-index:8");
-        inputControl3.Attributes.Add("name", "selectHealthFacility");
-        inputControl3.Attributes.Add("Visible", "false");
-        inputControl3.Attributes.Add("title", "Health Facility option description");
-
-        inputControl6 = new HtmlGenericControl("select");
-        inputControl6.Attributes.Add("class", "form-control");
-
-
-        DateTime currentDateTime = DateTime.Now;
-
-        for(int i=0;i<40;i++)
-        {
-            int p=-1*i;
-            DateTime d = currentDateTime.AddMonths(p);
-            string monthString = d.ToString("MMM")+ " - " + d.Year;
-            var opt = new HtmlGenericControl("option");
-            inputControl6.Controls.Add(opt);
-            opt.Attributes.Add("value", currentDateTime.ToString());
-            opt.InnerText = monthString;
-
-            if(selectedHealthFacilityID.Equals(currentDateTime.ToString())){
-                opt.Attributes.Add("selected","true");
-            }
-
-        }
-
-
-        inputControl6.Attributes.Add("id", "selectReportingPeriod");
-        inputControl6.Attributes.Add("type", "text");
-        inputControl6.Attributes.Add("style", "z-index:8");
-        inputControl6.Attributes.Add("name", "selectHealthFacility");
-        inputControl6.Attributes.Add("Visible", "false");
-        inputControl6.Attributes.Add("title", "Health Facility option description");
-
-
-        //date-from controls
-        var inputControl = new HtmlGenericControl("input");
-        inputControl.Attributes.Add("class", "form-control");
-
-        inputControl.Attributes.Add("id", "dateFrom");
-        inputControl.Attributes.Add("type", "text");
-        inputControl.Attributes.Add("style", "z-index:8");
-        inputControl.Attributes.Add("name", "dateFrom");
-        inputControl.Attributes.Add("value", datefromString);
-        inputControl.Attributes.Add("title", "Defaulters Report Description");
-
-        //date-to Controls
-        var inputControl2 = new HtmlGenericControl("input");
-        inputControl2.Attributes.Add("class", "form-control");
-
-        inputControl2.Attributes.Add("id", "dateTo");
-        inputControl2.Attributes.Add("type", "text");
-        inputControl2.Attributes.Add("style", "z-index:8");
-        inputControl2.Attributes.Add("name", "dateTo");
-        inputControl2.Attributes.Add("value", datetoString);
-        inputControl2.Attributes.Add("title", "Defaulters Report Description");
-
-        // Label control
-        var labelControl = new Label()
-        {
-            Text = "Report Period"
-        };
-
-            var labelControl2 = new Label()
-        {
-            Text = "To"
-        };
-
-            var labelControl3 = new Label()
-        {
-            Text = "District"
-        };
-
-        var row = new HtmlGenericControl("div");
-        row.Attributes.Add("class", "row");
-        // row.Attributes.Add("style", "margin:5px");
-
-        var colMd4 = new HtmlGenericControl("div");
-        colMd4.Attributes.Add("class", "col-md-3");
-        var colMd42 = new HtmlGenericControl("div");
-        colMd42.Attributes.Add("class", "col-md-1");
-        var colMd43 = new HtmlGenericControl("div");
-        colMd43.Attributes.Add("class", "col-md-1");
-
-        var colMd8 = new HtmlGenericControl("div");
-        colMd8.Attributes.Add("class", "col-md-5");
-        var colMd82 = new HtmlGenericControl("div");
-        colMd82.Attributes.Add("class", "col-md-3");
-        var colMd83 = new HtmlGenericControl("div");
-        colMd83.Attributes.Add("class", "col-md-3");
-
-        row.Controls.Add(colMd4);
-        row.Controls.Add(colMd8);
-        row.Controls.Add(colMd43);
-        row.Controls.Add(colMd83);
-
-        colMd4.Controls.Add(labelControl);
-        colMd8.Controls.Add(inputControl6);
-        colMd42.Controls.Add(labelControl2);
-        colMd82.Controls.Add(inputControl2);
-        colMd43.Controls.Add(labelControl3);
-        colMd83.Controls.Add(inputControl3);
-
-        this.reportInputs.Controls.Add(row);
-
-        Page.RegisterStartupScript("dateFrom",
-                    string.Format(
-                    "<script type=\"text/javascript\">Sys.Application.add_init(function() {{$create(Sys.Extended.UI.CalendarBehavior, {{\"format\":\"MM-dd-yyyy\",\"id\":\"dateFrom\"}}, null, null, $get(\"{0}\"));}});</script>", "dateFrom"));
-        Page.RegisterStartupScript("dateTo",
-                    string.Format(
-                    "<script type=\"text/javascript\">Sys.Application.add_init(function() {{$create(Sys.Extended.UI.CalendarBehavior, {{\"format\":\"MM-dd-yyyy\",\"id\":\"dateTo\"}}, null, null, $get(\"{0}\"));}});</script>", "dateTo"));
-                    //  $create(Sys.Extended.UI.CalendarBehavior, {"endDate":"Thu, 28 May 2015 00:00:00 GMT","format":"dd/MM/yyyy","id":"ctl00_ContentPlaceHolder1_ceBirthdateTo"}, null, null, $get("ctl00_ContentPlaceHolder1_txtBirthdateTo"));
-        
-    }
 
     protected void btnSearch_Click(object sender, EventArgs e)
     {
@@ -278,7 +244,7 @@ public partial class Pages_HealthFacilityListVaccinationSummary : System.Web.UI.
         selectedHealthFacilityID = Request.Form["selectHealthFacility"];
 
         odsHealthFacilityListVaccinationSummary.SelectParameters.Clear();
-        odsHealthFacilityListVaccinationSummary.SelectParameters.Add("districtCouncilId", selectedHealthFacilityID);
+        odsHealthFacilityListVaccinationSummary.SelectParameters.Add("hfid", selectedHealthFacilityID);
         odsHealthFacilityListVaccinationSummary.SelectParameters.Add("fromDate", strFromDate);
         odsHealthFacilityListVaccinationSummary.SelectParameters.Add("toDate", strToDate);
         odsHealthFacilityListVaccinationSummary.DataBind();
